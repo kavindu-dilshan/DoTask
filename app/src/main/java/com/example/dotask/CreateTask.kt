175b8210ace355dfc4fa4f20601e04d337355d23
Kavindu Dilshan <kavindudilshan84@gmail.com>
2024-05-12 11:09:15 +0530

    design layouts

---
+++ app/src/main/java/com/example/dotask/CreateTask.kt
@@ -0,0 +1,11 @@
+package com.example.dotask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_task)
+    }
+}
